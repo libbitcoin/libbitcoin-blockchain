eb65dfeabe81d6f355fded06085e1df860488aa6
Amir Taaki <genjix@riseup.net>
2014-07-09 16:23:48 +0200

    linked_records uses record_allocator and implements on disk 1-way linked list.

---
+++ include/bitcoin/blockchain/database/linked_records.hpp
@@ -0,0 +1,69 @@
+/*
+ * Copyright (c) 2011-2013 libbitcoin developers (see AUTHORS)
+ *
+ * This file is part of libbitcoin.
+ *
+ * libbitcoin is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License with
+ * additional permissions to the one published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option)
+ * any later version. For more information see LICENSE.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef LIBBITCOIN_BLOCKCHAIN_LINKED_RECORDS_HPP
+#define LIBBITCOIN_BLOCKCHAIN_LINKED_RECORDS_HPP
+
+#include <bitcoin/blockchain/database/record_allocator.hpp>
+
+namespace libbitcoin {
+    namespace chain {
+
+/**
+ * linked_records is a one-way linked list with a next value containing
+ * the index of the next record.
+ * Records can be dropped by forgetting an index, and updating to the next
+ * value. We can think of this as a LIFO queue.
+ */
+class linked_records
+{
+public:
+    static constexpr index_type empty = std::numeric_limits<index_type>::max();
+
+    BCB_API linked_records(record_allocator& allocator);
+
+    /**
+     * Create new list with a single record.
+     */
+    BCB_API index_type create();
+
+    /**
+     * Insert new record before index. Returns index of new record.
+     */
+    BCB_API index_type insert(index_type next);
+
+    /**
+     * Read next index for record in list.
+     */
+    BCB_API index_type next(index_type index);
+
+    /**
+     * Get underlying record data.
+     */
+    BCB_API record_type get(index_type index); 
+
+private:
+    record_allocator& allocator_;
+};
+
+    } // namespace chain
+} // namespace libbitcoin
+
+#endif
+
---
+++ src/database/linked_records.cpp
@@ -0,0 +1,66 @@
+/*
+ * Copyright (c) 2011-2013 libbitcoin developers (see AUTHORS)
+ *
+ * This file is part of libbitcoin.
+ *
+ * libbitcoin is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License with
+ * additional permissions to the one published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option)
+ * any later version. For more information see LICENSE.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+#include <bitcoin/blockchain/database/linked_records.hpp>
+
+#include <bitcoin/utility/assert.hpp>
+#include <bitcoin/utility/serializer.hpp>
+
+namespace libbitcoin {
+    namespace chain {
+
+linked_records::linked_records(record_allocator& allocator)
+  : allocator_(allocator)
+{
+}
+
+index_type linked_records::create()
+{
+    // Insert new record with empty next value.
+    return insert(empty);
+}
+
+index_type linked_records::insert(index_type next)
+{
+    // Create new record.
+    index_type record = allocator_.allocate();
+    uint8_t* data = allocator_.get(record);
+    // Write next value at first 4 bytes of record.
+    BITCOIN_ASSERT(sizeof(index_type) == 4);
+    auto serial = make_serializer(data);
+    serial.write_4_bytes(next);
+    return record;
+}
+
+index_type linked_records::next(index_type index)
+{
+    uint8_t* data = allocator_.get(index);
+    auto deserial = make_deserializer(data, data + 4);
+    index_type next = deserial.read_4_bytes();
+    return next;
+}
+
+record_type linked_records::get(index_type index)
+{
+    return allocator_.get(index) + 4;
+}
+
+    } // namespace chain
+} // namespace libbitcoin
+
