48eb4665665dc6a7773d12a2937cba6b68001ce4
Amir Taaki <genjix@riseup.net>
2014-07-16 03:21:56 +0200

    ./htdb_bench -w and validate_data()

--- benchmark/htdb_bench.cpp
+++ benchmark/htdb_bench.cpp
@@ -4,7 +4,7 @@
 using namespace libbitcoin;
 using namespace libbitcoin::chain;
 
-constexpr size_t total_txs = 200;
+constexpr size_t total_txs = 20000;
 constexpr size_t tx_size = 200;
 constexpr size_t buckets = 100;
 
@@ -22,7 +22,7 @@ void write_data()
     touch_file("htdb_slabs");
     mmfile file("htdb_slabs");
     BITCOIN_ASSERT(file.data());
-    file.resize(4 + 8 * buckets + 8);
+    file.resize(4 + 8 * buckets + 8 + total_txs * tx_size * 2);
 
     htdb_slab_header header(file, 0);
     header.initialize_new(buckets);
@@ -47,6 +47,34 @@ void write_data()
     }
 }
 
+void validate_data()
+{
+    mmfile file("htdb_slabs");
+    BITCOIN_ASSERT(file.data());
+
+    htdb_slab_header header(file, 0);
+    header.start();
+
+    BITCOIN_ASSERT(header.size() == buckets);
+
+    slab_allocator alloc(file, 4 + 8 * header.size());
+    alloc.start();
+
+    htdb_slab<hash_digest> ht(header, alloc);
+
+    std::default_random_engine engine;
+    for (size_t i = 0; i < total_txs; ++i)
+    {
+        data_chunk value = generate_random_bytes(engine, tx_size);
+        hash_digest key = bitcoin_hash(value);
+
+        const slab_type slab = ht.get(key);
+        BITCOIN_ASSERT(slab);
+
+        BITCOIN_ASSERT(std::equal(value.begin(), value.end(), slab));
+    }
+}
+
 void read_data()
 {
     mmfile file("htdb_slabs");
@@ -62,7 +90,11 @@ void read_data()
 
     htdb_slab<hash_digest> ht(header, alloc);
 
-    timed_section t("ht.get()", "200");
+    std::ostringstream oss;
+    oss << "txs = " << total_txs << " size = " << tx_size
+        << " buckets = " << buckets << " |  ";
+
+    timed_section t("ht.get()", oss.str());
     std::default_random_engine engine;
     for (size_t i = 0; i < total_txs; ++i)
     {
@@ -70,15 +102,36 @@ void read_data()
         hash_digest key = bitcoin_hash(value);
 
         const slab_type slab = ht.get(key);
-        BITCOIN_ASSERT(slab);
-
-        BITCOIN_ASSERT(std::equal(value.begin(), value.end(), slab));
     }
 }
 
-int main()
+void show_usage()
 {
-    write_data();
+    std::cerr << "Usage: htdb_bench [-w]" << std::endl;
+}
+
+int main(int argc, char** argv)
+{
+    if (argc != 1 && argc != 2)
+    {
+        show_usage();
+        return -1;
+    }
+    const std::string arg = (argc == 2) ? argv[1] : "";
+    if (arg == "-h" || arg == "--help")
+    {
+        show_usage();
+        return 0;
+    }
+    if (arg == "-w" || arg == "--write")
+    {
+        std::cout << "Writing..." << std::endl;
+        write_data();
+        std::cout << "Validating..." << std::endl;
+        validate_data();
+        std::cout << "Done." << std::endl;
+    }
+    // Perform benchmark.
     read_data();
     return 0;
 }
