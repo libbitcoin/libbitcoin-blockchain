28c52f36765b23864070987433e80e9f1d3f78ee
Amir Taaki <genjix@riseup.net>
2014-08-04 11:54:06 +0200

    added easier drop_caches.c that doesn't need root password (useful for long automatic benchmark tests)

---
+++ benchmark/drop_caches.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int main()
+{        
+    // Run this on binary after:
+    // sudo chmod 4755 drop_caches
+    setuid(0);
+    clearenv();
+    system("sync");
+    system("echo 1 > /proc/sys/vm/drop_caches");
+}
+
--- benchmark/drop_caches.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-sync
-echo 1 > /proc/sys/vm/drop_caches
-
