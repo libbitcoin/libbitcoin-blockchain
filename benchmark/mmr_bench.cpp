0704005eae5aaabba5cc37ef44312fd69a11498f
Amir Taaki <genjix@riseup.net>
2014-08-04 18:11:14 +0200

    add mmr_bench stub

---
+++ benchmark/mmr_bench.cpp
@@ -0,0 +1,12 @@
+#include <boost/lexical_cast.hpp>
+#include <bitcoin/bitcoin.hpp>
+#include <bitcoin/blockchain/database/htdb_slab.hpp>
+#include <bitcoin/utility/timed_section.hpp>
+using namespace libbitcoin;
+using namespace libbitcoin::chain;
+
+int main(int argc, char** argv)
+{
+    return 0;
+}
+
