b6aa367f6fedaa067b837c3a12adc3e18acf08b2
Amir Taaki <genjix@riseup.net>
2014-08-18 11:56:24 +0200

    block_database

---
+++ include/bitcoin/blockchain/database/block_database.hpp
@@ -0,0 +1,135 @@
+/*
+ * Copyright (c) 2011-2013 libbitcoin developers (see AUTHORS)
+ *
+ * This file is part of libbitcoin.
+ *
+ * libbitcoin is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License with
+ * additional permissions to the one published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option)
+ * any later version. For more information see LICENSE.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef LIBBITCOIN_BLOCKCHAIN_BLOCK_DATABASE_HPP
+#define LIBBITCOIN_BLOCKCHAIN_BLOCK_DATABASE_HPP
+
+#include <bitcoin/error.hpp>
+#include <bitcoin/primitives.hpp>
+#include <bitcoin/blockchain/define.hpp>
+#include <bitcoin/blockchain/database/htdb_slab.hpp>
+#include <bitcoin/blockchain/database/record_allocator.hpp>
+#include <bitcoin/blockchain/database/types.hpp>
+
+namespace libbitcoin {
+    namespace chain {
+
+class block_result
+{
+public:
+    block_result(const slab_type slab);
+
+    /**
+     * Test whether the result exists, return false otherwise.
+     */
+    BCB_API operator bool() const;
+
+    /**
+     * Read block header.
+     */
+    BCB_API block_header_type header() const;
+
+    /**
+     * Read the number of transactions in this block.
+     */
+    BCB_API size_t transactions_size() const;
+
+    /**
+     * Read a transaction_index where i < transactions_size().
+     */
+    BCB_API index_type transaction_index(size_t i) const;
+
+private:
+    const slab_type slab_;
+};
+
+typedef std::vector<index_type> transaction_index_list;
+
+/**
+ * Stores block_headers each with a list of transaction indexes.
+ * Lookup possible by hash or height.
+ */
+class block_database
+{
+public:
+    block_database(
+        const std::string& map_filename, const std::string& index_filename);
+
+    /**
+     * Initialize a new transaction database.
+     */
+    BCB_API void initialize_new();
+
+    /**
+     * You must call start() before using the database.
+     */
+    BCB_API void start();
+
+    /**
+     * Fetch block by height using the index table.
+     */
+    BCB_API block_result get(const size_t height) const;
+
+    /**
+     * Fetch block by hash using the hashtable.
+     */
+    BCB_API block_result get(const hash_digest& hash) const;
+
+    /**
+     * Store a block in the database.
+     */
+    BCB_API void store(const size_t height, const block_header_type& header,
+        const transaction_index_list& tx_indexes);
+
+    /**
+     * Unlink all blocks upwards from (and including) from_height.
+     */
+    BCB_API void unlink(const size_t from_height);
+
+    /**
+     * Synchronise storage with disk so things are consistent.
+     * Should be done at the end of every block write.
+     */
+    BCB_API void sync();
+
+private:
+    typedef htdb_slab<hash_digest> map_type;
+
+    /// Write position of tx and return the height (assigned index);
+    index_type write_position(const position_type position);
+    /// Use intermediate records table to find blk position from height.
+    position_type read_position(const index_type index) const;
+
+    /// The hashtable used for looking up blocks by hash.
+    mmfile map_file_;
+    htdb_slab_header header_;
+    slab_allocator allocator_;
+    map_type map_;
+
+    /// Table used for looking up blocks by height.
+    /// Resolves to a position within the slab.
+    mmfile index_file_;
+    record_allocator index_;
+};
+
+    } // namespace chain
+} // namespace libbitcoin
+
+#endif
+
---
+++ src/database/block_database.cpp
@@ -0,0 +1,165 @@
+/*
+ * Copyright (c) 2011-2013 libbitcoin developers (see AUTHORS)
+ *
+ * This file is part of libbitcoin.
+ *
+ * libbitcoin is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License with
+ * additional permissions to the one published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option)
+ * any later version. For more information see LICENSE.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+#include <bitcoin/blockchain/database/block_database.hpp>
+
+#include <bitcoin/satoshi_serialize.hpp>
+#include <bitcoin/block.hpp>
+#include <bitcoin/blockchain/database/sizes.hpp>
+
+namespace libbitcoin {
+    namespace chain {
+
+constexpr size_t number_buckets = 10000;
+constexpr size_t header_size = htdb_slab_header_size(number_buckets);
+constexpr size_t initial_map_file_size = header_size + min_slab_size;
+
+constexpr position_type alloc_offset = header_size;
+
+template <typename Iterator>
+block_header_type deserialize(const Iterator first)
+{
+    block_header_type header;
+    satoshi_load<Iterator, false>(first, nullptr, header);
+    return header;
+}
+
+block_result::block_result(const slab_type slab)
+  : slab_(slab)
+{
+}
+
+block_result::operator bool() const
+{
+    return slab_ != nullptr;
+}
+
+block_header_type block_result::header() const
+{
+    BITCOIN_ASSERT(slab_);
+    return deserialize(slab_);
+}
+
+size_t block_result::transactions_size() const
+{
+    BITCOIN_ASSERT(slab_);
+    return from_little_endian<uint32_t>(slab_ + 80);
+}
+
+index_type block_result::transaction_index(size_t i) const
+{
+    BITCOIN_ASSERT(slab_);
+    BITCOIN_ASSERT(i < transactions_size());
+    const uint8_t* first = slab_ + 80 + 4 + i * 4;
+    return from_little_endian<uint32_t>(first);
+}
+
+block_database::block_database(
+    const std::string& map_filename, const std::string& index_filename)
+  : map_file_(map_filename), header_(map_file_, 0),
+    allocator_(map_file_, alloc_offset), map_(header_, allocator_),
+    index_file_(index_filename),
+    index_(index_file_, 0, sizeof(position_type))
+{
+    BITCOIN_ASSERT(map_file_.data());
+    BITCOIN_ASSERT(index_file_.data());
+}
+
+void block_database::initialize_new()
+{
+    map_file_.resize(initial_map_file_size);
+    header_.initialize_new(number_buckets);
+    allocator_.initialize_new();
+
+    index_file_.resize(min_records_size);
+    index_.initialize_new();
+}
+
+void block_database::start()
+{
+    header_.start();
+    allocator_.start();
+    index_.start();
+}
+
+block_result block_database::get(const size_t height) const
+{
+    if (height >= index_.size())
+        return block_result(nullptr);
+    const position_type position = read_position(height);
+    const slab_type slab = allocator_.get(position);
+    return block_result(slab);
+}
+
+block_result block_database::get(const hash_digest& hash) const
+{
+    const slab_type slab = map_.get(hash);
+    return block_result(slab);
+}
+
+void block_database::store(
+    const size_t height, const block_header_type& header,
+    const transaction_index_list& tx_indexes)
+{
+    // Write block data.
+    const hash_digest key = hash_block_header(header);
+    const size_t value_size = 80 + 4 + tx_indexes.size() * 4;
+    auto write = [&header, &tx_indexes](uint8_t* data)
+    {
+        satoshi_save(header, data);
+        auto serial = make_serializer(data + 80);
+        serial.write_4_bytes(tx_indexes.size());
+        for (const index_type tx_index: tx_indexes)
+            serial.write_4_bytes(tx_index);
+    };
+    const position_type position = map_.store(key, value_size, write);
+    // Write height -> position mapping.
+    const index_type index = write_position(position);
+    BITCOIN_ASSERT(index == height);
+}
+
+void block_database::unlink(const size_t from_height)
+{
+    index_.resize(from_height);
+}
+
+void block_database::sync()
+{
+    allocator_.sync();
+    index_.sync();
+}
+
+index_type block_database::write_position(const position_type position)
+{
+    const index_type index = index_.allocate();
+    record_type record = index_.get(index);
+    auto serial = make_serializer(record);
+    serial.write_8_bytes(position);
+    return index;
+}
+
+position_type block_database::read_position(const index_type index) const
+{
+    record_type record = index_.get(index);
+    return from_little_endian<position_type>(record);
+}
+
+    } // namespace chain
+} // namespace libbitcoin
+
