df98b8534ece6cc9431bea18c317a4642026cf43
Amir Taaki <genjix@riseup.net>
2014-08-28 00:30:34 +0100

    add sum_difficulty to simple_chain_impl

--- src/impl/simple_chain_impl.cpp
+++ src/impl/simple_chain_impl.cpp
@@ -17,10 +17,11 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
+#include "simple_chain_impl.hpp"
+
 #include <bitcoin/format.hpp>
 #include <bitcoin/transaction.hpp>
 #include <bitcoin/utility/logger.hpp>
-#include "simple_chain_impl.hpp"
 
 namespace libbitcoin {
     namespace chain {
@@ -48,25 +49,15 @@ int simple_chain_impl::find_index(const hash_digest& search_block_hash)
 
 hash_number simple_chain_impl::sum_difficulty(size_t begin_index)
 {
-#if 0
     hash_number total_work = 0;
-    leveldb_iterator it(db_.block->NewIterator(leveldb::ReadOptions()));
-    auto raw_height = to_little_endian(begin_index);
-    for (it->Seek(slice(raw_height)); it->Valid(); it->Next())
+    const size_t last_height = interface_.blocks.last_height();
+    BITCOIN_ASSERT(last_height != block_database::null_height);
+    for (size_t i = begin_index; i <= last_height; ++i)
     {
-        constexpr size_t bits_field_offset = 4 + 2 * hash_size + 4;
-        BITCOIN_ASSERT(it->value().size() >= 84);
-        // Deserialize only the bits field of block header.
-        const char* bits_field_begin = it->value().data() + bits_field_offset;
-        std::string raw_bits(bits_field_begin, 4);
-        auto deserial = make_deserializer(raw_bits.begin(), raw_bits.end());
-        uint32_t bits = deserial.read_4_bytes();
-        // Accumulate the total work.
+        uint32_t bits = interface_.blocks.get(i).header().bits;
         total_work += block_work(bits);
     }
     return total_work;
-#endif
-    return hash_number();
 }
 
 bool simple_chain_impl::release(size_t begin_index,
