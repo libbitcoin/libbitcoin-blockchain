8336c7c7cea89f612070b4ec56e91c27f905feff
evoskuil <eric@voskuil.org>
2015-05-25 23:42:29 -0700

    Flatten, use const, whitespace.

--- src/blockchain.cpp
+++ src/blockchain.cpp
@@ -19,41 +19,48 @@
  */
 #include <bitcoin/blockchain/blockchain.hpp>
 
+#include <cstdint>
+#include <memory>
+#include <system_error>
+
 namespace libbitcoin {
 namespace chain {
 
 using std::placeholders::_1;
 using std::placeholders::_2;
 
-/**
- * Fast modulus calculation where divisor is a power of 2.
- */
+// Fast modulus calculation where divisor is a power of 2.
 uint64_t remainder_fast(const hash_digest& value, const uint64_t divisor)
 {
     BITCOIN_ASSERT(divisor % 2 == 0);
+
     // Only use the first 8 bytes of hash value for this calculation.
-    uint64_t hash_value = from_little_endian_unsafe<uint64_t>(value.begin());
+    const auto hash_value = from_little_endian_unsafe<uint64_t>(value.begin());
+
     // x mod 2**n == x & (2**n - 1)
     return hash_value & (divisor - 1);
 }
 
 uint64_t spend_checksum(output_point outpoint)
 {
-    // Assuming outpoint hash is sufficiently random,
-    // this method should work well for generating row checksums.
-    // Maximum pow2 value for a uint64_t is 1 << 63
+    // Assuming outpoint hash is sufficiently random, this method works well
+    // for generating row checksums. Max pow2 value for a uint64_t is 1 << 63.
     constexpr uint64_t divisor = uint64_t{1} << 63;
     static_assert(divisor == 9223372036854775808ull, "Wrong divisor value.");
+
     // Write index onto outpoint hash.
     auto serial = make_serializer(outpoint.hash.begin());
     serial.write_4_bytes(outpoint.index);
-    // Collapse it into uint64_t
+
+    // Collapse it into uint64_t.
     return remainder_fast(outpoint.hash, divisor);
 }
 
 // fetch_block
 typedef blockchain_fetch_handler_block handler_block;
 
+// TODO: move to private header: fetch_block_t.hpp.
+// TODO: rename to block_fetcher.
 class fetch_block_t
   : public std::enable_shared_from_this<fetch_block_t>
 {
@@ -64,17 +71,18 @@ public:
     template <typename BlockIndex>
     void start(const BlockIndex& index, handler_block handle)
     {
+        const auto handle_fetch_header = [this](const std::error_code& ec,
+            const block_header_type& block_header)
+        {
+            if (stop_on_error(ec))
+                return;
+
+            block_.header = block_header;
+            fetch_hashes();
+        };
+
         handle_ = handle;
-        auto this_ptr = shared_from_this();
-        chain_.fetch_block_header(index,
-            [this, this_ptr](const std::error_code& ec,
-                const block_header_type& block_header)
-            {
-                if (stop_on_error(ec))
-                    return;
-                block_.header = block_header;
-                fetch_hashes();
-            });
+        chain_.fetch_block_header(index, handle_fetch_header);
     }
 
 private:
@@ -82,12 +90,14 @@ private:
     {
         if (stopped_)
             return true;
-        else if (ec)
+
+        if (ec)
         {
             stopped_ = true;
             handle_(ec, block_type());
             return true;
         }
+
         return false;
     }
 
@@ -104,36 +114,33 @@ private:
     {
         if (stop_on_error(ec))
             return;
+
         block_.transactions.resize(tx_hashes.size());
         count_ = 0;
         for (size_t tx_index = 0; tx_index < tx_hashes.size(); ++tx_index)
-        {
             fetch_tx(tx_hashes[tx_index], tx_index);
-        }
     }
 
     void fetch_tx(const hash_digest& tx_hash, size_t tx_index)
     {
-        auto this_ptr = shared_from_this();
-        chain_.fetch_transaction(tx_hash,
-            [this, this_ptr, tx_index](
-                const std::error_code& ec,
-                const transaction_type& tx)
-            {
-                if (stop_on_error(ec))
-                    return;
-                BITCOIN_ASSERT(tx_index < block_.transactions.size());
-                block_.transactions[tx_index] = tx;
-                if (++count_ == block_.transactions.size())
-                {
-                    handle_(std::error_code(), block_);
-                }
-            });
+        const auto handle_fetch = [this, tx_index](
+            const std::error_code& ec, const transaction_type& tx)
+        {
+            if (stop_on_error(ec))
+                return;
+
+            BITCOIN_ASSERT(tx_index < block_.transactions.size());
+            block_.transactions[tx_index] = tx;
+            ++count_;
+            if (count_ == block_.transactions.size())
+                handle_(std::error_code(), block_);
+        };
+
+        chain_.fetch_transaction(tx_hash, handle_fetch);
     }
 
     blockchain& chain_;
     handler_block handle_;
-
     block_type block_;
     atomic_counter count_;
     bool stopped_;
@@ -142,19 +149,22 @@ private:
 void fetch_block(blockchain& chain, size_t height,
     handler_block handle_fetch)
 {
-    auto fetcher = std::make_shared<fetch_block_t>(chain);
+    const auto fetcher = std::make_shared<fetch_block_t>(chain);
     fetcher->start(height, handle_fetch);
 }
+
 void fetch_block(blockchain& chain, const hash_digest& block_hash,
     handler_block handle_fetch)
 {
-    auto fetcher = std::make_shared<fetch_block_t>(chain);
+    const auto fetcher = std::make_shared<fetch_block_t>(chain);
     fetcher->start(block_hash, handle_fetch);
 }
 
 // fetch_block_locator
 typedef blockchain_fetch_handler_block_locator handler_locator;
 
+// TODO: move to private header: fetch_locator.hpp.
+// TODO: rename to locator_fetcher.
 class fetch_locator
   : public std::enable_shared_from_this<fetch_locator>
 {
@@ -165,7 +175,7 @@ public:
     void start(handler_locator handle)
     {
         handle_ = handle;
-        auto this_ptr = shared_from_this();
+        const auto this_ptr = shared_from_this();
         chain_.fetch_last_height(
             std::bind(&fetch_locator::populate,
                 this_ptr, _1, _2));
@@ -179,6 +189,7 @@ private:
             handle_(ec, block_locator_type());
             return true;
         }
+
         return false;
     }
 
@@ -186,7 +197,9 @@ private:
     {
         if (stop_on_error(ec))
             return;
+
         indexes_ = block_locator_indexes(last_height);
+
         // We reverse our list so we can pop items off the top
         // as we need to get them, and push items to our locator.
         // The order of items in the locator should match
@@ -203,20 +216,24 @@ private:
             handle_(std::error_code(), locator_);
             return;
         }
-        auto this_ptr = shared_from_this();
-        auto height = indexes_.back();
+
+        const auto this_ptr = shared_from_this();
+        const auto height = indexes_.back();
         indexes_.pop_back();
         chain_.fetch_block_header(height,
-            std::bind(&fetch_locator::append, this_ptr, _1, _2, height));
+            std::bind(&fetch_locator::append,
+                this_ptr, _1, _2, height));
     }
 
-    void append(const std::error_code& ec,
-        const block_header_type& blk_header, size_t /* height */)
+    void append(const std::error_code& ec, const block_header_type& blk_header,
+        size_t /* height */)
     {
         if (stop_on_error(ec))
             return;
-        auto block_hash = hash_block_header(blk_header);
+
+        const auto block_hash = hash_block_header(blk_header);
         locator_.push_back(block_hash);
+
         // Continue the loop.
         loop();
     }
