062120c35d76e91303d43ca0ad7a3eb9d509317c
evoskuil <eric@voskuil.org>
2015-12-22 07:41:34 -0800

    Integrate libbitcoin/libsecp256k1/version4

--- install.sh
+++ install.sh
@@ -219,7 +219,8 @@ BOOST_OPTIONS_CLANG=\
 # Define secp256k1 options.
 #------------------------------------------------------------------------------
 SECP256K1_OPTIONS=\
-"--disable-tests "
+"--disable-tests "\
+"--enable-module-recovery "
 
 # Define bitcoin options.
 #------------------------------------------------------------------------------
@@ -552,9 +553,9 @@ build_from_travis()
 build_all()
 {
     build_from_tarball_boost $BOOST_URL $BOOST_ARCHIVE boost $PARALLEL $BOOST_OPTIONS
-    build_from_github libbitcoin secp256k1 version3 $PARALLEL "$@" $SECP256K1_OPTIONS
+    build_from_github libbitcoin secp256k1 version4 $PARALLEL "$@" $SECP256K1_OPTIONS
     build_from_github libbitcoin libbitcoin sync $PARALLEL "$@" $BITCOIN_OPTIONS
-    build_from_github libbitcoin libbitcoin-consensus version1 $PARALLEL "$@" $BITCOIN_CONSENSUS_OPTIONS
+    build_from_github libbitcoin libbitcoin-consensus version2 $PARALLEL "$@" $BITCOIN_CONSENSUS_OPTIONS
     build_from_travis libbitcoin libbitcoin-blockchain sync $PARALLEL "$@" $BITCOIN_BLOCKCHAIN_OPTIONS
 }
 
