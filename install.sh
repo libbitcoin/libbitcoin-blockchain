729e67dcc75dc01e2ac029242c4fc7792d70c044
evoskuil <eric@voskuil.org>
2016-02-04 15:39:14 -0800

    Change installer to build from master (vs. sync).

--- install.sh
+++ install.sh
@@ -562,9 +562,9 @@ build_all()
 {
     build_from_tarball_boost $BOOST_URL $BOOST_ARCHIVE boost $PARALLEL $BOOST_OPTIONS
     build_from_github libbitcoin secp256k1 version4 $PARALLEL "$@" $SECP256K1_OPTIONS
-    build_from_github libbitcoin libbitcoin sync $PARALLEL "$@" $BITCOIN_OPTIONS
+    build_from_github libbitcoin libbitcoin master $PARALLEL "$@" $BITCOIN_OPTIONS
     build_from_github libbitcoin libbitcoin-consensus version2 $PARALLEL "$@" $BITCOIN_CONSENSUS_OPTIONS
-    build_from_travis libbitcoin libbitcoin-blockchain sync $PARALLEL "$@" $BITCOIN_BLOCKCHAIN_OPTIONS
+    build_from_travis libbitcoin libbitcoin-blockchain master $PARALLEL "$@" $BITCOIN_BLOCKCHAIN_OPTIONS
 }
 
 
