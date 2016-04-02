2ba601d0b2a28a83713af6f3efb52ac10fb0e8f6
evoskuil <eric@voskuil.org>
2016-04-01 19:27:42 -0700

    Installer fixes and updates.

--- libbitcoin_blockchain_test_runner.sh
+++ libbitcoin_blockchain_test_runner.sh
@@ -18,4 +18,4 @@ BOOST_UNIT_TEST_OPTIONS=\
 
 # Run tests.
 #==============================================================================
-./test/libbitcoin_blockchain_test ${BOOST_UNIT_TEST_OPTIONS}
+./test/libbitcoin_blockchain_test ${BOOST_UNIT_TEST_OPTIONS} > test.log
