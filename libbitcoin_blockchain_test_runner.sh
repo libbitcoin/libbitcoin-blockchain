32952b1ce8c75095d914c44bb672fae46eba2420
evoskuil <eric@voskuil.org>
2016-11-03 22:52:47 -0700

    Run all tests.

--- libbitcoin_blockchain_test_runner.sh
+++ libbitcoin_blockchain_test_runner.sh
@@ -9,7 +9,7 @@
 # Define tests and options.
 #==============================================================================
 BOOST_UNIT_TEST_OPTIONS=\
-"--run_test=fork_tests,transaction_pool_tests,validate_block_tests "\
+"--run_test=* "\
 "--show_progress=no "\
 "--detect_memory_leak=0 "\
 "--report_level=no "\
