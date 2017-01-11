171c424c4cd5d3cf93fa0e015194b49141a767f0
evoskuil <eric@voskuil.org>
2017-01-11 04:45:07 -0800

    Stub in block_pool tests.

---
+++ test/block_pool.cpp
@@ -0,0 +1,36 @@
+/**
+ * Copyright (c) 2011-2015 libbitcoin developers (see AUTHORS)
+ *
+ * This file is part of libbitcoin.
+ *
+ * libbitcoin is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License with
+ * additional permissions to the one published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option)
+ * any later version. For more information see LICENSE.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+#include <boost/test/unit_test.hpp>
+
+#include <bitcoin/blockchain.hpp>
+
+using namespace bc;
+using namespace bc::blockchain;
+
+BOOST_AUTO_TEST_SUITE(block_pool_tests)
+
+// construct
+
+BOOST_AUTO_TEST_CASE(block_pool__construct__zero__does_not_throw)
+{
+    block_pool instance(0);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
