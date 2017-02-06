cfcb5bcd6299485600652360b0ab0d4e789282fa
evoskuil <eric@voskuil.org>
2017-02-06 14:39:08 -0800

    Add tx cache efficiency to block info logging.

--- include/bitcoin/blockchain/validate/validate_block.hpp
+++ include/bitcoin/blockchain/validate/validate_block.hpp
@@ -75,7 +75,7 @@ private:
         result_handler handler) const;
     void connect_inputs(block_const_ptr block, size_t bucket,
         size_t buckets, result_handler handler) const;
-    void handle_connected(const code& ec, size_t height,
+    void handle_connected(const code& ec, block_const_ptr block,
         result_handler handler) const;
 
     // These are thread safe.
