a9ca82875e0799ad9da81ee118ac63d4ba7d964e
evoskuil <eric@voskuil.org>
2017-04-06 00:10:33 -0700

    Change fetch_history to not accept version byte (hash only).

--- include/bitcoin/blockchain/interface/safe_chain.hpp
+++ include/bitcoin/blockchain/interface/safe_chain.hpp
@@ -124,9 +124,8 @@ public:
     virtual void fetch_spend(const chain::output_point& outpoint,
         spend_fetch_handler handler) const = 0;
 
-    virtual void fetch_history(const wallet::payment_address& address,
-        size_t limit, size_t from_height,
-        history_fetch_handler handler) const = 0;
+    virtual void fetch_history(const short_hash& address_hash, size_t limit,
+        size_t from_height, history_fetch_handler handler) const = 0;
 
     virtual void fetch_stealth(const binary& filter, size_t from_height,
         stealth_fetch_handler handler) const = 0;
