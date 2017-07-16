feea72365b5a4fd92e206dc20b1298575810be06
evoskuil <eric@voskuil.org>
2017-07-16 12:02:01 -0700

    Don't skip allow collisions in pool state population (opitmization fix).

--- src/populate/populate_chain_state.cpp
+++ src/populate/populate_chain_state.cpp
@@ -160,12 +160,6 @@ bool populate_chain_state::populate_checkpoint(chain_state::data& data,
         return true;
     }
 
-    if (is_transaction_pool(branch))
-    {
-        data.allow_collisions_hash = null_hash;
-        return true;
-    }
-
     return get_block_hash(data.allow_collisions_hash,
         map.allow_collisions_height, branch);
 }
