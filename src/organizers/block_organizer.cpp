25138c47386ab299c72ff353bf4590b34252974b
evoskuil <eric@voskuil.org>
2018-10-28 15:05:16 -0700

    Remove logging from critical section.

--- src/organizers/block_organizer.cpp
+++ src/organizers/block_organizer.cpp
@@ -127,8 +127,10 @@ bool block_organizer::handle_check(const code& ec, const hash_digest& hash,
     ///////////////////////////////////////////////////////////////////////////
     mutex_.lock_high_priority();
 
+    const auto start_height = height - 1u;
+
     // If initial height is misaligned try again on next download.
-    if (fast_chain_.top_valid_candidate_state()->height() != height - 1u)
+    if (fast_chain_.top_valid_candidate_state()->height() != start_height)
     {
         //---------------------------------------------------------------------
         mutex_.unlock_high_priority();
@@ -137,19 +139,16 @@ bool block_organizer::handle_check(const code& ec, const hash_digest& hash,
 
     // Stack up the validated blocks for possible reorganization.
     auto branch_cache = std::make_shared<block_const_ptr_list>();
-    auto branch_height = height;
+    auto end_height = start_height;
     code error_code;
 
-    for (auto branch_height = height; !stopped() && height != 0; ++height)
+    for (;!stopped() && height != 0; ++height)
     {
         // TODO: check last downloaded cache first (for fast top validation).
         // TODO: create parallel block reader (this is expensive and serial).
         // TODO: this can run in the block populator using priority dispatch.
         // TODO: consider metadata population in line with block read.
         auto block = fast_chain_.get_block(height, true, true);
-
-        LOG_DEBUG(LOG_BLOCKCHAIN)
-            << "Got next block #" << height;
 
         // If hash is misaligned we must be looking at an expired notification.
         if (!block || fast_chain_.top_valid_candidate_state()->hash() !=
@@ -183,12 +182,10 @@ bool block_organizer::handle_check(const code& ec, const hash_digest& hash,
                 break;
         }
 
-        LOG_DEBUG(LOG_BLOCKCHAIN)
-            << "Validated block #" << height << " ["
-            << encode_hash(block->hash()) << "]";
-
         if (fast_chain_.is_reorganizable())
         {
+            const auto branch_height = height - (branch_cache->size() - 1u);
+
             // Reorganize this stronger candidate branch into confirmed chain.
             //#################################################################
             error_code = fast_chain_.reorganize(branch_cache, branch_height);
@@ -197,22 +194,22 @@ bool block_organizer::handle_check(const code& ec, const hash_digest& hash,
             if (error_code)
                 break;
 
-            LOG_INFO(LOG_BLOCKCHAIN)
-                << "Organized blocks [" << branch_height << "-"
-                << branch_height + branch_cache->size() - 1u << "]";
-
             // Reset the branch for next reorganization.
-            branch_height += branch_cache->size();
+            end_height = branch_height;
             branch_cache->clear();
         }
-
-        // Top valid chain state should have been updated to match the block.
-        BITCOIN_ASSERT(fast_chain_.top_valid_candidate_state()->height() ==
-            height);
     }
 
     mutex_.unlock_high_priority();
     ///////////////////////////////////////////////////////////////////////////
+
+    const auto count = end_height - start_height;
+    if (count != 0 && (count % 10) == 0)
+    {
+        LOG_INFO(LOG_BLOCKCHAIN)
+            << "Organized " << count << " blocks ["
+            << (start_height + 1u) << "-" << end_height << "]";
+    }
 
     // A non-stop error result implies store corruption.
     if (error_code && error_code != error::service_stopped)
