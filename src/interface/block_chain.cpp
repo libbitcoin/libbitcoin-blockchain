301c96846c1c8bbcceddceebb71fcbfb16380596
evoskuil <eric@voskuil.org>
2018-11-02 16:45:55 -0700

    Remove comments.

--- src/interface/block_chain.cpp
+++ src/interface/block_chain.cpp
@@ -447,9 +447,6 @@ code block_chain::reorganize(const config::checkpoint& fork,
         set_candidate_work(0);
     }
 
-    LOG_INFO(LOG_BLOCKCHAIN)
-        << "Reorganized [" << incoming->size() << "] headers.";
-
     set_top_candidate_state(top_state);
     notify(fork_height, incoming, outgoing);
     return ec;
@@ -755,9 +752,6 @@ void block_chain::set_confirmed_work(const uint256_t& work_above_fork)
 // private.
 void block_chain::set_top_candidate_state(chain::chain_state::ptr top)
 {
-    LOG_INFO(LOG_BLOCKCHAIN)
-        << "set_top_candidate_state: [" << encode_hash(top->hash()) << "]";
-
     top_candidate_state_.store(top);
 }
 
@@ -815,10 +809,6 @@ chain::chain_state::ptr block_chain::chain_state(const chain::header& header,
 chain::chain_state::ptr block_chain::promote_state(const chain::header& header,
     chain::chain_state::ptr parent) const
 {
-    LOG_INFO(LOG_BLOCKCHAIN)
-        << "Block: [" << encode_hash(header.hash()) << "] Parent: ["
-        << encode_hash(parent->hash()) << "]";
-
     if (!parent || parent->hash() != header.previous_block_hash())
         return {};
 
